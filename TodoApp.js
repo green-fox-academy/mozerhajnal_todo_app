f20e5a70086f920c1c753a46e7050b3baa22702f
Hazsni <mozerhajni@gmail.com>
2021-11-25 20:48:59 +0100

    second task completed

--- TodoApp.js
+++ TodoApp.js
@@ -1,21 +1,40 @@
 export default class TodoApp{
 
-    constructor(argv){
-        this.argv = argv;
+    constructor(args){
+        this.args = args;
+
     }
 
-    message = `
-Parancssori Todo applikáció
-=============================
-    
-Parancssori argumentumok:
-    -l   Kilistázza a feladatokat
-    -a   Új feladatot ad hozzá
-    -r   Eltávolít egy feladatot
-    -c   Teljesít egy feladatot`;
+    print(string){
+        console.log(string);
+    }
 
-    init(){
-        console.log(this.message);
+    printUserGuide(){
+        const message =
+        'Parancssori Todo applikáció\n\
+        =============================\n\
+        \n\
+        Parancssori argumentumok:\n\
+        -l   Kilistázza a feladatokat\n\
+        -a   Új feladatot ad hozzá\n\
+        -r   Eltávolít egy feladatot\n\
+        -c   Teljesít egy feladatot';
+        
+        return this.print(message);      
+
+        
+    }
+
+    list(tasks){
+        tasks.forEach((element, index) => {
+            console.log(index+1, '-', element);
+        })
+    }
+
+    run(tasks){
+        if (this.args.includes('-l')) {
+            this.list(tasks);
+        }
     }
 
 }
--- app.js
+++ app.js
@@ -1,6 +1,20 @@
 import TodoApp from "./TodoApp.js";
-
 const argv = process.argv.slice(2);
+import fs from 'fs';
 
 const app = new TodoApp(argv);
-app.init();
+console.log('$ todo: \n');
+app.printUserGuide();
+
+let fileContent;
+try {
+    fileContent = fs.readFileSync('./data/todos.json', 'utf-8');
+} catch (err) {
+    console.error(err);
+}
+const parsedContent = JSON.parse(fileContent);
+
+console.log('\n$ todo -l:\n');
+
+app.run(parsedContent);
+

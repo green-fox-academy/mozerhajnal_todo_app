4f44a247133cd764d4d18a19edca80b048ce7a50
Hazsni <mozerhajni@gmail.com>
2021-11-26 23:30:50 +0100

    remove task invalid argument handling

--- TodoApp.js
+++ TodoApp.js
@@ -1,21 +1,21 @@
 import Todo from "./Todo.js"
-import {saveInFile} from "./fileoperation.js"
+import { saveInFile } from "./fileoperation.js"
 
-export default class TodoApp{
+export default class TodoApp {
     todoList = [];
 
-    constructor(args){
+    constructor(args) {
         this.args = args;
 
     }
 
-    print(string){
+    print(string) {
         console.log(string);
     }
 
-    printUserGuide(){
+    printUserGuide() {
         const message =
-        'Parancssori Todo applikáció\n\
+            'Parancssori Todo applikáció\n\
         =============================\n\
         \n\
         Parancssori argumentumok:\n\
@@ -23,73 +23,81 @@ export default class TodoApp{
         -a   Új feladatot ad hozzá\n\
         -r   Eltávolít egy feladatot\n\
         -c   Teljesít egy feladatot';
-        
-        return this.print(message);      
-        
+
+        return this.print(message);
+
     }
 
-    saveInitValues(todo){
+    saveInitValues(todo) {
         todo.forEach(element => {
-            if(this.todoList.indexOf(element) > -1){}
-                this.todoList.push(element);
+            if (this.todoList.indexOf(element) > -1) { }
+            this.todoList.push(element);
         });
     }
 
-    list(){
-        if(this.todoList.length === 0){
+    list() {
+        if (this.todoList.length === 0) {
             console.log("Nincs mára tennivalód! :)");
         }
         this.todoList.forEach((element, index) => {
-            console.log(index+1, '-', element.task);
+            console.log(index + 1, '-', element.task);
         })
     }
 
-    addNewToDo(){
-        if(this.args.length === 1){
-            console.log("Nem lehetséges új feladat hozzáadása: nincs megadva a feladat!");
+    addNewToDo() {
+        if (this.args.length === 1) {
+            return console.log("Nem lehetséges új feladat hozzáadása: nincs megadva a feladat!");
         }
         this.args.filter((element, index) => {
-            if(index === 1){
+            if (index === 1) {
                 this.todoList.push(new Todo(element));
                 saveInFile(this.todoList);
-                console.log(`${element} was added!`);
+                return console.log(`${element} was added!`);
             }
         });
     }
 
-    completeTask(){
+    completeTask() {
         this.args.filter((element, index) => {
-            if(index === 1){
-                this.todoList[element-1].status = 1;
+            if (index === 1) {
+                this.todoList[element - 1].status = 1;
                 saveInFile(this.todoList);
-                console.log(`${this.todoList[element-1].task} completed!`);
+                console.log(`${this.todoList[element - 1].task} completed!`);
             }
         });
     }
 
-    removeTask(){
+    removeTask() {
+        if (this.args.length === 1) {
+            return console.log("Nem lehetséges az eltávolítás: nem adott meg indexet!");
+        }
         this.args.filter((element, index) => {
-            if(index === 1){
-                const removedItem = this.todoList[element-1].task;
-                this.todoList.splice([element-1],1);
+            if (index === 1) {
+                if (element > this.todoList.length) {
+                    return console.log("Nem lehetséges az eltávolítás: túlindexelési probléma adódott!");
+                } else if (isNaN(element)) {
+                    return console.log("Nem lehetséges az eltávolítás: a megadott index nem szám!")
+                }
+                const removedItem = this.todoList[element - 1].task;
+                this.todoList.splice([element - 1], 1);
                 saveInFile(this.todoList);
-                console.log(`${removedItem} removed!`);
+                return console.log(`${removedItem} removed!`);
             }
         });
     }
 
-    run(){
-        if(!this.args.length){ 
+    run() {
+        if (!this.args.length) {
             this.printUserGuide();
-        }else if (this.args.includes('-l')) {
+        } else if (this.args.includes('-l')) {
             this.list();
-        }else if(this.args.includes('-a')){
+        } else if (this.args.includes('-a')) {
             this.addNewToDo();
-        }else if(this.args.includes('-c')){
+        } else if (this.args.includes('-c')) {
             this.completeTask();
-        }else if(this.args.includes('-r')){
+        } else if (this.args.includes('-r')) {
             this.removeTask();
-        }else{
+        } else {
             console.log("Nem támogatott argumentum!");
             this.printUserGuide();
         }
